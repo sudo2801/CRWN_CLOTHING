83a4bea83281d1e199fe35315f3994843fa94272
imparag28 <paragj2801@gmail.com>
2022-03-11 10:36:47 +0530

    Heading Start

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -4,8 +4,8 @@ import "./homepage.styles.scss"
 
 const HomePage = () => {
     return(
-        <div className="homepage">
-        <div className="directory">
+     <div className="homepage">
+        <div className=".directory-menu">
             <div className="menu-item">
                 <div className="content">
                     <h1 className="title">HATS</h1>
